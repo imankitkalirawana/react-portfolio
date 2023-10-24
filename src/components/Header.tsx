c1075c101dc13cbec205c768f946b924a49beb49
imankitkalirawana <009ankitkalirawana@gmail.com>
2023-10-24 19:55:30 +0530

    added mobile sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,19 @@ function Header() {
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  // !Sidebar
+
+  const [sidebar, setSidebar] = useState("-100%");
+
+  const openSidebar = () => {
+    setSidebar("0%");
+  };
+
+  const closeSidebar = () => {
+    setSidebar("-100%");
   };
 
   return (
@@ -55,28 +67,20 @@ function Header() {
                 </div>
               </Link>
               <ul className="header-menu hide-header">
-                <Link to="/#">
-                  <li className="menu-item">
-                    <p>Home</p>
-                  </li>
+                <Link to="/#" className="menu-item">
+                  <p>Home</p>
                 </Link>
-                <Link to="#skills">
-                  <li className="menu-item">
-                    <p>Skills</p>
-                  </li>
+                <Link to="#skills" className="menu-item">
+                  <p>Skills</p>
                 </Link>
-                <Link to="#about">
-                  <li className="menu-item">
-                    <p>About</p>
-                  </li>
+                <Link to="#about" className="menu-item">
+                  <p>About</p>
                 </Link>
-                <Link to="#contact">
-                  <li className="menu-item">
-                    <p>Contact</p>
-                  </li>
+                <Link to="#contact" className="menu-item">
+                  <p>Contact</p>
                 </Link>
                 <li className="menu-item menu-theme">
-                  <span>Theme</span>
+                  Theme
                   <div className="theme-dropdown">
                     <span
                       id="light-theme-toggle-btn"
@@ -111,85 +115,68 @@ function Header() {
                   Create an account
                 </a>
               </div>
-              <button className="header-menu-open" id="header-menu-open">
+              <button className="header-menu-open" onClick={openSidebar}>
                 <i className="fa-duotone fa-bars-staggered"></i>
               </button>
             </div>
           </div>
         </header>
       </div>
-      {/* <div
-        id="header-side"
-        className="header-side"
-        style={{ top: sliderPosition }}
-      >
-        <div className="header-side-top">
-          <div className="header-logo">
+      <div className="sidebar" style={{ transform: `translateX(${sidebar})` }}>
+        <div className="sidebar-header">
+          <Link to="/#" className="sidebar-brand">
             <i className="fa-duotone fa-code"></i>
             <div className="brand-text">
               <span className="text-1">Divinely</span>
-              <span className="text-2">Developers</span>
+              <span className="text-2">Developer</span>
             </div>
-          </div>
-          <button
-            className="header-menu-close"
-            id="header-menu-close"
-            onClick={closeSlider}
-          >
-            <i className="fa-duotone fa-circle-xmark"></i>
+          </Link>
+          <button className="sidebar-close" onClick={closeSidebar}>
+            <i className="fa-duotone fa-times"></i>
           </button>
         </div>
-
-        <div className="header-side-center">
-          <ul className="header-menu">
-            <Link to="#" className="menu-item">
-              <a>Home</a>
-            </Link>
-            <Link to="#about" className="menu-item">
-              <a>About</a>
-            </Link>
+        <ul className="sidebar-menu">
+          <Link to="/#">
             <li className="menu-item">
-              <a href="#">Skills</a>
+              <i className="fa-solid fa-house"></i>
+              <p>Home</p>
             </li>
+          </Link>
+          <Link to="#skills">
             <li className="menu-item">
-              <a href="#">Contact</a>
+              <i className="fa-solid fa-code"></i>
+              <p>Skills</p>
             </li>
-            <li className="menu-item menu-theme">
-              <a>
-                <label
-                  className="theme-switch head_dropdown_item"
-                  htmlFor="checkbox"
-                >
-                  <span className="head_dropdown_item-title">
-                    <span id="appearance">
-                      {theme === "dark" ? "Light Theme" : "Dark Theme"}
-                    </span>
-                    <span className="slider round"></span>
-                  </span>
-
-                  <input
-                    type="checkbox"
-                    className="checkbox"
-                    id="checkbox"
-                    checked={theme == "dark"}
-                    onChange={toggleTheme}
-                    hidden
-                  />
-                </label>
-              </a>
+          </Link>
+          <Link to="#about">
+            <li className="menu-item">
+              <i className="fa-solid fa-user"></i>
+              <p>About</p>
             </li>
-          </ul>
-        </div>
+          </Link>
+          <Link to="#contact">
+            <li className="menu-item">
+              <i className="fa-solid fa-envelope"></i>
+              <p>Contact</p>
+            </li>
+          </Link>
+          <li className="menu-item menu-theme">
+            <span>Light</span>
+            <div className="toggle-btn">
+              <input
+                type="checkbox"
+                id="switch"
+                hidden
+                checked={theme === "dark"}
+                onClick={toggleTheme}
+              />
 
-        <div className="header-side-bottom btns">
-          <a href="#" className="btn btn-primary">
-            Login
-          </a>
-          <a href="#" className="btn btn-primary">
-            Create an account
-          </a>
-        </div>
-      </div> */}
+              <label htmlFor="switch">Toggle</label>
+            </div>
+            <span>Dark</span>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
